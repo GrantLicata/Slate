e2b35ede985e4e81113a956c852b6cb8936bd0f1
GrantLicata <grant.licata@gmail.com>
2022-12-06 18:04:34 -0800

    Prep for user main view

---
+++ client/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+
+        {/* Users notes and other related data will go here */}
+
+    </div>
+  )
+}
+
+export default Main
